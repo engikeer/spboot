63990a508dfdd7a1503849b7dceab83d31e7a384
engikeer <engikeer@github.com>
2020-03-24 20:16:48 +0800

    update.

---
+++ src/test/java/com/mfun/spboot/SpbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mfun.spboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
