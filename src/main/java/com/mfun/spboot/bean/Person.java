e1592273f9c2043fdd81748a44b2092da9d2e402
engikeer <engikeer@github.com>
2020-03-25 16:23:23 +0800

    update.

---
+++ src/main/java/com/mfun/spboot/bean/Person.java
@@ -0,0 +1,91 @@
+package com.mfun.spboot.bean;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+@Component
+@ConfigurationProperties(prefix = "person")
+public class Person implements Serializable {
+    private String name;
+    private Integer age;
+    private Boolean boss;
+    private Date birthDate;
+
+    private Map<String, Object> map;
+    private List<String> list;
+    private Dog dog;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", isBoss=" + boss +
+                ", birthDate=" + birthDate +
+                ", map=" + map +
+                ", list=" + list +
+                ", dog=" + dog +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Boolean getBoss() {
+        return boss;
+    }
+
+    public void setBoss(Boolean boss) {
+        this.boss = boss;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Map<String, Object> getMap() {
+        return map;
+    }
+
+    public void setMap(Map<String, Object> map) {
+        this.map = map;
+    }
+
+    public List<String> getList() {
+        return list;
+    }
+
+    public void setList(List<String> list) {
+        this.list = list;
+    }
+
+    public Dog getDog() {
+        return dog;
+    }
+
+    public void setDog(Dog dog) {
+        this.dog = dog;
+    }
+}
--- src/test/java/com/mfun/spboot/SpbootApplicationTests.java
+++ src/test/java/com/mfun/spboot/SpbootApplicationTests.java
@@ -1,13 +1,19 @@
 package com.mfun.spboot;
 
+import com.mfun.spboot.bean.Person;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpbootApplicationTests {
 
+    @Autowired
+    private Person person;
+
     @Test
     void contextLoads() {
+        System.out.println(person);
     }
 
 }
