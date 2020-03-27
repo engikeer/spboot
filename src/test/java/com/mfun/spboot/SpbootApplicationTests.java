bd49a5ad7c230dd66e0f74c113a77f38db9b6744
engikeer <engikeer@github.com>
2020-03-27 12:02:49 +0800

    update.

--- src/test/java/com/mfun/spboot/SpbootApplicationTests.java
+++ src/test/java/com/mfun/spboot/SpbootApplicationTests.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 
