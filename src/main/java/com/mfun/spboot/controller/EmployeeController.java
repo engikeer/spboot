823d50c313f1f111256c56fc1d12b041c4d28f30
engikeer <engikeer@github.com>
2020-03-30 17:05:03 +0800

    update.

--- src/main/java/com/mfun/spboot/controller/EmployeeController.java
+++ src/main/java/com/mfun/spboot/controller/EmployeeController.java
@@ -4,9 +4,10 @@ import com.mfun.spboot.bean.Department;
 import com.mfun.spboot.bean.Employee;
 import com.mfun.spboot.dao.DepartmentDao;
 import com.mfun.spboot.dao.EmployeeDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 import java.util.Map;
@@ -16,6 +17,7 @@ public class EmployeeController {
 
     private final EmployeeDao employeeDao;
     private final DepartmentDao departmentDao;
+    Logger logger = LoggerFactory.getLogger(getClass());
 
     public EmployeeController(EmployeeDao employeeDao, DepartmentDao departmentDao) {
         this.employeeDao = employeeDao;
@@ -39,8 +41,33 @@ public class EmployeeController {
 
     @PostMapping("/emp")
     public String addEmp(Employee employee) {
-        System.out.println(employee);
+        logger.debug("添加员工：" + employee);
         employeeDao.save(employee);
+        return "redirect:/emps";
+    }
+
+    @GetMapping("/emp/{id}")
+    public String toEditPage(@PathVariable Integer id,
+                             Map<String, Object> map) {
+        Employee employee = employeeDao.getOne(id);
+        Collection<Department> departments = departmentDao.getAll();
+        map.put("emp", employee);
+        map.put("depts", departments);
+        // 去 add 页面进行修改
+        return "emp/add";
+    }
+
+    @PutMapping("/emp")
+    public String updateEmp(Employee employee) {
+        logger.debug("修改员工：" + employee);
+        employeeDao.save(employee);
+        return "redirect:/emps";
+    }
+
+    @DeleteMapping("/emp/{id}")
+    public String deleteEmp(@PathVariable Integer id) {
+        logger.debug("删除员工：" + id);
+        employeeDao.delete(id);
         return "redirect:/emps";
     }
 }
