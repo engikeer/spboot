70a812fa132db2f2279cc89fd79a6839b9db13c1
engikeer <engikeer@github.com>
2020-03-31 17:52:20 +0800

    update.

--- src/main/java/com/mfun/spboot/config/MyServletConfig.java
+++ src/main/java/com/mfun/spboot/config/MyServletConfig.java
@@ -61,7 +61,7 @@ public class MyServletConfig {
 
 class MyServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/html");
         resp.setCharacterEncoding("UTF-8");
         try (PrintWriter writer = resp.getWriter()) {
